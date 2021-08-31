c23f77325bd1bcf85a9cd418236c21bdf4df8e84
Francisco <francisco.marquez.solt@gmail.com>
2021-08-31 17:13:16 -0500

    Probar Actions commit

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,8 @@
+import actions from '../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe("Actions", () =>{
+  test("addToCart Action", () => {
+    const payload = ProductMock
+  })
+})
