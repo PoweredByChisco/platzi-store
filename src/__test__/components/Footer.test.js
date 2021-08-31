93e17c540bb6a880f1b5bf6e911f65efb300aaa4
Francisco <francisco.marquez.solt@gmail.com>
2021-08-31 13:22:21 -0500

    Crear Mocks

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -4,8 +4,12 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer/>', () => {
+  const footer = mount(<Footer />);
   test('Render of the component Footer', () => {
-    const footer = mount(<Footer />);
-    expect(footer.lenght).toEqual(1);
+    expect(footer.length).toEqual(1);
   });
+  test('Title render', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  }); /* Verifica si el texto de esta clase es el de Platzi Store */
+  
 });
